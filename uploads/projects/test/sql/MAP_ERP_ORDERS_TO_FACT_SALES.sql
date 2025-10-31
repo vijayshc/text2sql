f50d265aed1ababa54dbfc9c4f5169ca3548adf7
vijay <vijay.sch@gmail.com>
2025-10-31 09:29:37 +0800

    fix

--- uploads/projects/test/sql/MAP_ERP_ORDERS_TO_FACT_SALES.sql
+++ uploads/projects/test/sql/MAP_ERP_ORDERS_TO_FACT_SALES.sql
@@ -1,3 +1,5 @@
 -- Generated Code for MAP_ERP_ORDERS_TO_FACT_SALES
--- Generated at: 2025-10-31T00:23:39.639797
+-- Generation Timestamp: 2025-10-31T00:23:39.639797
 
+BEGIN
+  /* 1️⃣  Prepare source data using CTEs
